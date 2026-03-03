6d8343164271f41d483a43a57ff316732df17325
Giacomo Capone <giacomo.capone.25@stud.itsaltoadriatico.it>
2026-03-03 10:12:29 +0100

    conteggio parole

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 with open("input.txt", "r") as f:
-    righe = f.readlines()
+    contenuto = f.read()
     f.close()
+    righe = contenuto.splitlines()
+    parole = contenuto.split()
 print("numero totale di righe: {len(righe)}")
+print("numero totale di parole: {len(parole)}")
